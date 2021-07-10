ab693c2c77bb9c10bd8455dd139d639919ccc23d
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-07-10 08:55:03 -0300

    Añade 'GrupoInput, Input, LeyendaError, IconoValidacion'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import './App.css'
-import { Formulario, Label } from './elementos/Formularios'
+import { Formulario, Label, GrupoInput, Input, LeyendaError,IconoValidacion } from './elementos/Formularios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle,faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <main>
       <Formulario action="">
         <div>
-          <Label htmlFor="">Usuario</Label>
-          <div>
-            <input type="text" placeholder="Usuario" />
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <Label htmlFor="nombre">Usuario</Label>
+          <GrupoInput>
+            <Input type="text" placeholder="Usuario" id="nombre" />
+            <IconoValidacion icon={faCheckCircle}/>
+          </GrupoInput>
+          <LeyendaError>Lorem ipsum dolor sit amet.</LeyendaError>
         </div>
         <div>
           <Label htmlFor="">Usuario</Label>
-          <div>
-            <input type="text" placeholder="Usuario" />
-          </div>
-          <p>Lorem ipsum dolor sit amet.</p>
+          <GrupoInput>
+            <Input type="text" placeholder="Usuario" />
+          </GrupoInput>
+          <LeyendaError>Lorem ipsum dolor sit amet.</LeyendaError>
+        </div>
+        <div>
+          <Label>
+            <input type="checkbox" name="terminos" id="terminos" />Acepto los terminos y condiciones
+          </Label>
+        </div>
+        <div>
+          <p>
+            <FontAwesomeIcon icon={faExclamationTriangle} />
+            <b>Error:</b> Por favor rellene el formulario correctamente
+          </p>
+        </div>
+        <div>
+          <button type="submit" >Enviar</button>
+          <p>Formulario enviado correctamente!</p>
         </div>
       </Formulario>
     </main>
--- src/elementos/Formularios.js
+++ src/elementos/Formularios.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 const colores = {
     borde:"#0075FF",
@@ -20,4 +22,48 @@ const Label = styled.label `
         min-heigh:40px;
         cursor: pointer;
 `;
-export {Formulario,Label};
+const GrupoInput = styled.div `
+        position: relative;
+        z-index: 90;
+`;
+const Input = styled.input`
+        width: 100%;
+        background: #fff;
+        border-radius: 3px;
+        height: 45px;
+        line-height: 45px;
+        padding: 0 40px 0 10px;
+        transition: .3s ease all;
+        border: 3px solid transparent;
+
+        &:focus{
+            border: 3px solid ${colores.borde};
+            outline: none;
+            box-shadow: 3px 0px 30px rgba(163,163,163, 0.4)
+        }
+`;
+const LeyendaError = styled.p`
+        font-size: 12px;
+        margin-bottom: 0;
+        color: ${colores.error};
+
+`;
+const IconoValidacion = styled(FontAwesomeIcon)`
+        position: absolute;
+        right: 10px;
+        bottom: 14px;
+        z-index: 100;
+        font-size: 16px;
+`;
+
+
+
+
+
+
+
+
+
+
+
+export {Formulario,Label, GrupoInput, Input, LeyendaError, IconoValidacion};
